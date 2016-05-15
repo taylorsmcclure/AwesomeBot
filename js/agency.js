77789dfed6bb11c0e9895cf3586934728b6fb205
BitQuote <BitQuote@users.noreply.github.com>
2016-05-15 03:54:17 -0400

    Removed awfulbot easter egg

--- js/agency.js
+++ js/agency.js
@@ -20,26 +20,15 @@ $('body').scrollspy({
     target: '.navbar-fixed-top'
 })
 
+// Enable popovers
+$(function () {
+  $('[data-toggle="popover"]').popover()
+})
+
 // Closes the Responsive Menu on Menu Item Click
 $('.navbar-collapse ul li a').click(function() {
     $('.navbar-toggle:visible').click();
 });
-
-var awesome = true;
-function switchNames() {
-    if(awesome) {
-        $("body").contents().each(function () {
-            if (this.nodeType === 3) this.nodeValue = $.trim($(this).text()).replace(/AwesomeBot/g, "AwfulBot")
-            if (this.nodeType === 1) $(this).html( $(this).html().replace(/AwesomeBot/g, "AwfulBot") )
-        })
-    } else {
-        $("body").contents().each(function () {
-            if (this.nodeType === 3) this.nodeValue = $.trim($(this).text()).replace(/AwfulBot/g, "AwesomeBot")
-            if (this.nodeType === 1) $(this).html( $(this).html().replace(/AwfulBot/g, "AwesomeBot") )
-        })
-    }
-    awesome = !awesome;
-}
 
 function showStatus() {
     getJSON("https://awesomebot-botmakersinc.rhcloud.com/data?section=list&type=bot", function(data) {
